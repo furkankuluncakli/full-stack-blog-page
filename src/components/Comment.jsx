5f8b22af3d1e69bc54987cf71e779b6b91936f2a
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-09-28 18:22:02 +0300

    Refactor Comment and Comments components for improved structure and styling

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Image from "./Image";
 
 function Comment() {
   return (
-    <div>Comment</div>
-  )
+    <div className="flex flex-col gap-4 bg-slate-50 p-4 rounded-xl">
+      <div className="flex items-center gap-4">
+        <Image src="userImg.jpeg" className="w-12 h-12 rounded-full object-cover" w="40"/>
+        <span className="font-medium">John Doe</span>
+        <span className="text-sm text-gray-500">2 Days Ago</span>
+      </div>
+      <div className="mt-1">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique animi iste, esse dolores, illo rerum totam sapiente eaque ex sunt obcaecati repellendus cumque, nisi aliquid nobis. Dolorem esse perferendis eligendi maiores necessitatibus accusantium. Dolores nulla corrupti fuga sapiente quis fugiat recusandae dolore eligendi eius nesciunt voluptatem officiis, ad odit blanditiis nam quas? Asperiores quaerat sint modi sequi vel, placeat suscipit, maxime quasi laudantium repellendus iure facere culpa perspiciatis doloribus autem eaque dolor soluta ab explicabo quae odio et. Iure, perferendis!</p>
+      </div>
+    </div>
+  );
 }
 
-export default Comment
+export default Comment;
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,3 +1,5 @@
+import Comment from "./Comment";
+
 function Comments() {
   return (
     <div className="flex flex-col gap-8 lg:w-3/5">
@@ -5,14 +7,18 @@ function Comments() {
       <div className="flex items-center justify-between gap-8 w-full">
         <textarea
           placeholder="Write a comment"
-          className="w-full p-4 rounded-xl"
+          className="w-full p-4 rounded-xl outline-none"
         />
 
         <button className="px-4 py-2 bg-blue-500 text-white rounded-xl">
           Send
         </button>
       </div>
-      
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
     </div>
   );
 }
